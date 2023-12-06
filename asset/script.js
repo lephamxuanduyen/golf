0d24a72bfe0dfe95826916273f957d1e5610c21e
Lê Phạm Xuân Duyên <117836247+lephamxuanduyen@users.noreply.github.com>
2023-12-06 23:38:43 +0700

    done

--- asset/script.js
+++ asset/script.js
@@ -1,3 +1,5 @@
+// Animation move chuột
+
 var crsr = document.querySelector("#cursor")
 var crsr_blur = document.querySelector("#cursor-blur")
 
@@ -7,6 +9,11 @@ document.addEventListener("mousemove",function(dets){
     crsr_blur.style.left = dets.x-250+"px"
     crsr_blur.style.top = dets.y-250+"px"
 })
+
+window.addEventListener('scroll',changeNavBg);
+
+////////////////////////////////////////////////////////////
+// Animation đổi màu thanh Nav
 
 function changeNavBg(){
     var scrollValue = window.scrollY;
@@ -19,6 +26,9 @@ function changeNavBg(){
         nav.classList.add('bgNavBgColor')
     }
 }
+
+////////////////////////////////////////////////////////////
+// Đổi màu background
 
 function changeMainBg(){
     var scrollValue = window.scrollY
@@ -33,16 +43,21 @@ function changeMainBg(){
     }
 }
 
+window.addEventListener('scroll',changeMainBg)
+
+////////////////////////////////////////////////////////////
+// Animation Quotes
+
 function leftQuotes(){
     var scrollValue = window.scrollY
     var main = document.getElementById('colon1')
-    if (scrollValue < 1900){
+    if (scrollValue < 3400){
         main.classList.remove('changeColon1')
         main.classList.add('colon1')
     }
     else{
         main.classList.add('changeColon1')
-        main.classList.add('colon1')
+        main.classList.remove('colon1')
 
     }
 }
@@ -50,22 +65,27 @@ function leftQuotes(){
 function rightQuotes(){
     var scrollValue = window.scrollY
     var main = document.getElementById('colon2')
-    if (scrollValue < 1900){
+    if (scrollValue < 3500){
         main.classList.remove('changeColon2')
         main.classList.add('colon2')
     }
     else{
         main.classList.add('changeColon2')
-        main.classList.add('colon2')
+        main.classList.remove('colon2')
 
     }
 }
 
+window.addEventListener('scroll',leftQuotes)
+window.addEventListener('scroll',rightQuotes)
+
+//////////////////////////////////////////////////////////
+// Animation cho câu cuối trang
+
 function end(){
     var scrollValue = window.scrollY
     var main = document.getElementById('end')
-    console.log(scrollValue)
-    if (scrollValue < 2500){
+    if (scrollValue < 3800){
         main.classList.remove('changeEnd')
         main.classList.add('end')
     }
@@ -75,8 +95,61 @@ function end(){
     }
 }
 
-window.addEventListener('scroll',changeNavBg);
-window.addEventListener('scroll',changeMainBg)
-window.addEventListener('scroll',leftQuotes)
-window.addEventListener('scroll',rightQuotes)
 window.addEventListener('scroll',end)
+
+//////////////////////////////////////////////////////////
+
+// slider
+var slides = document.querySelectorAll('.slider')
+var btns = document.querySelectorAll('.btn')
+let current = 1
+
+var manualNav = function(manual){
+    slides.forEach((slide) => {
+        slide.classList.remove('active');
+        slide.classList.add('no-display')
+
+        btns.forEach((btn) => {
+            btn.classList.remove('active');
+            btn.classList.add('no-active');
+        });
+    });
+
+    slides[manual].classList.remove('no-display')
+    slides[manual].classList.add('active');
+    btns[manual].classList.add('active');
+    btns[manual].classList.remove('no-active');
+}
+
+btns.forEach((btn, i) => {
+    btn.addEventListener("click", () =>{
+        manualNav(i);
+        current = i;
+    });
+});
+
+setInterval(() => {
+    manualNav(current)
+    current++
+    if (current == slides.length){
+        current = 0
+    }
+}, 3000)
+
+//////////////////////////////////////////////////////////
+
+// Thanh nav
+
+var open_nav_btn = document.querySelector('.menu-btn')
+var close_nav_btn = document.querySelector('.close-nav-btn')
+var menuMobile = document.querySelector('.menu_mobile')
+
+open_nav_btn.addEventListener('click',() => {
+    menuMobile.classList.remove('no-active')
+    menuMobile.classList.add('active')
+})
+
+close_nav_btn.addEventListener('click', () => {
+    menuMobile.classList.remove('active')
+    menuMobile.classList.add('no-active')
+})
